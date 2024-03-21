eefb1f79320ba9f1852524bd88a791c23581f456
mamatbasreng88 <chikalverguson128@gmail.com>
2024-03-21 15:25:04 +0700

    firts comit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Hello world
+
+    <Buttonc />
+    <p><TextH /></p>
+    
+  </div>
+</template>
+
+<script setup>
+  import Buttonc from './components/Buttonc.vue'
+  import TextH from './components/Textheader.vue'
+</script>
---
+++ src/components/Buttonc.vue
@@ -0,0 +1,13 @@
+<template>
+    <button class="btn">
+        jamal
+    </button>
+</template>
+
+<style scoped>
+.btn{
+    background-color: blue;
+    color: black;
+    font-size: 28px;
+}
+</style>
